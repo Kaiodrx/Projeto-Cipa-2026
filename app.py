cf6a321d733b236c0fa8a873b1231c086deecd87
Kaio Lima <kaiolima292@gmail.com>
2026-04-18 16:32:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -304,7 +304,8 @@ def confirmacao():
     return render_template('votacao/confirmacao.html')
 
 
+with app.app_context():
+    init_db()
+
 if __name__ == '__main__':
-    with app.app_context():
-        init_db()
     app.run(debug=True)

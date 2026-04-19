19b939305d11141ed18b7a6f375aae56c4438f3e
Kaio Lima <kaiolima292@gmail.com>
2026-04-19 11:22:03 -0300

    Create constants.py

---
+++ app/constants.py
@@ -0,0 +1,11 @@
+UNIDADES = [
+    'UTD SOBRADINHO',
+    'UTD PLANALTINA',
+    'UTD SIA',
+    'SEDE PARK SHOPPING',
+    'UTD TAGUATINGA',
+    'UTD LAGO SUL',
+    'UTD SÃO SEBASTIÃO',
+]
+
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}

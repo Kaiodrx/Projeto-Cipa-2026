8d817e4ce53f5292eac62df93191b98f20bdfa3b
Kaio Lima <kaiolima292@gmail.com>
2026-05-11 20:22:02 -0300

    Update __init__.py

--- app/routes/admin/__init__.py
+++ app/routes/admin/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 
-from . import auth, dashboard, candidatos, funcionarios, eleicao, participacao, resultado, historico  # noqa
+from . import auth, dashboard, candidatos, funcionarios, eleicao, apuracao, relatorios, participacao, resultado, historico  # noqa
